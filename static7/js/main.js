4b4f63fc1b22a2cd83b69d222c971683a6cf54bf
kato2901 <seiten.hkss321@gmail.com>
2020-05-13 14:32:35 +0900

    フェードイン/フェードアウト

---
+++ static7/js/main.js
@@ -0,0 +1,48 @@
+// 未完
+var opacity = 1.0;
+var timer_id = null;
+const late = 50;
+
+function timer_f_in() {
+  if(timer_id == null) {
+    timer_id = setInterval(click_f_in, late);
+  }
+}
+
+function timer_f_out() {
+  if(timer_id == null) {
+    timer_id = setInterval(click_f_out, late);
+  }
+}
+
+function click_f_in() {
+  if(opacity < 1.0) {
+    opacity += 0.1;
+    document.getElementById("image").style.opacity = opacity;
+  } else {
+    clearInterval(timer_id);
+    timer_id = null;
+  }
+}
+
+function click_f_out() {
+  if(opacity > 0) {
+    opacity -= 0.1;
+    document.getElementById("image").style.opacity = opacity;
+  } else {
+    clearInterval(timer_id);
+    timer_id = null;
+  }
+}
+
+function click_org() {
+  clearInterval(timer_id);
+  opacity = 1.0;
+  document.getElementById("image").style.opacity = opacity;
+}
+
+window.onload = function() {
+  document.getElementById("f_in").addEventListener("click", click_f_in);
+  document.getElementById("f_out").addEventListener("click", click_f_out);
+  document.getElementById("org").addEventListener("click", click_org);
+}
